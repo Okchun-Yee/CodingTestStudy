67c553633f4c640645c4b4687ccf5704b33c5e68
HanChaeHoon <han020813@gmail.com>
2026-06-27 16:04:25 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    if(a%2 && b%2){
+        answer = (a * a) + (b * b);
+    }
+    else if(a%2 || b %2){
+        answer = 2 * (a + b);
+    }
+    else{
+        answer = abs(a - b);
+    }
+    return answer;
+}
