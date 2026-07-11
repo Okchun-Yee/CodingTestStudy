9f366b7ab5449da9ebdfeae7e4f99e851d56e200
HanChaeHoon <han020813@gmail.com>
2026-07-11 15:30:58 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map <string, int> m;
+
+    for(int i =0;i<clothes.size();i++){
+        m[clothes[i][1]]++;
+    }
+    for(auto& p : m){
+        answer *= (p.second + 1);
+    }
+    return answer - 1;
+}
